ebae7151116f08fc3011a98ad2231c6045c4bae1
RafaelS <rafasnascimento@hotmail.com>
2025-06-28 16:52:55 -0300

    Created Services for Plateau and Rover

---
+++ Domain/Services/PlateauService.cs
@@ -0,0 +1,42 @@
+﻿using ExploringMars.Entities;
+
+namespace Application.Services;
+
+public static class PlateauService
+{
+    public static Plateau CreatePlateau()
+    {
+        var specs = GetPlateauSpecs();
+
+        return Plateau.Create(specs.First(), specs.Last());
+    }
+
+    public static void GetExplorationResults(Plateau plateau)
+    {
+        if (plateau.Rovers.Count == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+        foreach(var rover in plateau.Rovers)
+        {
+
+        }
+    }
+
+    private static List<int> GetPlateauSpecs()
+    {
+        var plateauSpecs = Console.ReadLine();
+
+        if (plateauSpecs == string.Empty)
+            throw new ArgumentOutOfRangeException("Plateau specifications cannot be empty.");
+
+        var specs = plateauSpecs.Split(" ").Select(int.Parse).ToList();
+
+        if (specs == null || specs.Count != 2)
+            throw new ArgumentOutOfRangeException("Plateau must have two specifications.");
+
+        return specs;
+    }
+}
