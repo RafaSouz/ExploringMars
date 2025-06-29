39f50d25b3ddc94acaf1c473dbf4dac26b09e1a1
RafaelS <rafasnascimento@hotmail.com>
2025-06-28 22:47:47 -0300

    Created CommandHelper

---
+++ Application/Helpers/CommandHelper.cs
@@ -0,0 +1,21 @@
+﻿using ExploringMars.Entities.Enums;
+
+namespace Application.Helpers;
+
+public static class CommandHelper
+{
+    public static Command StringToCommand(char command)
+    {
+        switch (command)
+        {
+            case 'L':
+                return Command.L;
+            case 'R':
+                return Command.R;
+            case 'M':
+                return Command.M;
+            default:
+                return Command.N;
+        }
+    }
+}
