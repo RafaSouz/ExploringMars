12056a59649f7441a6ef67a8d87cefc3a7418c58
RafaelS <rafasnascimento@hotmail.com>
2025-06-28 23:21:33 -0300

    Extrated methods from CreateRoverList and added validation to rover's positions

--- Application/Services/RoverService.cs
+++ Application/Services/RoverService.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Helpers;
 using ExploringMars.Entities;
+using ExploringMars.Entities.Enums;
 
 namespace Application.Services;
 
@@ -7,30 +8,67 @@ public static class RoverService
 {
     public static void GetRoverPosition(this Rover rover) => Rover.GetPosition(rover);
 
-    public static List<Rover> CreateRoverList()
+    public static List<Rover> CreateRoverList(int x, int y)
     {
-        var roverList = new List<Rover>();
+        var rovers = new List<Rover>();
 
         do
         {
-            var positionSpecifications = Console.ReadLine();
+            var positionDetails = GetRoverInitialPosition();
 
-            if (positionSpecifications == null || positionSpecifications == string.Empty)
+            if (positionDetails == null)
                 break;
 
-            var positionDetais = positionSpecifications.Split(" ");
+            rovers.Add(Rover.Create(int.Parse(positionDetails[0]), int.Parse(positionDetails[1]), DirectionHelper.StringToCommand(positionDetails[2]), GetCommands()));
 
-            if (positionDetais.Count() != 3)
-                throw new ArgumentOutOfRangeException("Initial position of a rover must have only three specifications.");
-
-            var commandString = Console.ReadLine();
-
-            var commands = commandString.ToCharArray().Select(CommandHelper.StringToCommand).ToList();
-
-            roverList.Add(Rover.Create(int.Parse(positionDetais[0]), int.Parse(positionDetais[1]), DirectionHelper.StringToCommand(positionDetais[2]), commands));
+            ValidateInitialPosition(rovers, x, y);
         }
         while (true);
 
-        return roverList;
+        return rovers;
+    }
+
+    private static void ValidateInitialPosition(List<Rover> rovers, int x, int y)
+    {
+        foreach (var rover in rovers)
+        {
+            ValidateLimits(rover.Position, x, y);
+
+            ValidateFreePosition(rover.Position, rovers.Where(x => x.RoverId != rover.RoverId).Select(x => x.Position).ToList());
+        }
+    }
+
+    private static void ValidateFreePosition(Position position, List<Position> positions)
+    {
+        if(positions.Where(x => x.X == position.X &&  x.Y == position.Y).Any())
+            throw new ArgumentException("Initial rover position its already occupied.");
+    }
+
+    private static void ValidateLimits(Position position, int x, int y)
+    {
+        if (position.X > x || position.Y > y)
+            throw new ArgumentException("Initial rover position exceeds limites of plateau.");
+    }
+
+    private static List<Command> GetCommands()
+    {
+        var commandString = Console.ReadLine();
+
+        return commandString.ToCharArray().Select(CommandHelper.StringToCommand).ToList();
+    }
+
+    private static List<string> GetRoverInitialPosition()
+    {
+        var positionSpecifications = Console.ReadLine();
+
+        if (positionSpecifications == null || positionSpecifications == string.Empty)
+            return null;
+
+        var positionDetails = positionSpecifications.Split(" ");
+
+        if (positionDetails.Count() != 3)
+            throw new ArgumentOutOfRangeException("Initial position of a rover must have only three specifications.");
+
+        return positionDetails.ToList();
     }
 }
