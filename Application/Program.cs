b26347b99bd7eb783c9389d86f344011ebb7fe93
RafaelS <rafasnascimento@hotmail.com>
2025-06-28 23:22:24 -0300

    Finished program.cs logic and methods chain. Added exploration service to execute commands.

--- Application/Program.cs
+++ Application/Program.cs
@@ -2,6 +2,8 @@
 
 var plateau = PlateauService.CreatePlateau();
 
+plateau.Rovers = RoverService.CreateRoverList(plateau.Height, plateau.Width);
 
+ExplorationService.ExecuteRoversCommands(plateau);
 
 PlateauService.GetExplorationResults(plateau);
---
+++ Application/Services/ExplorationService.cs
@@ -0,0 +1,11 @@
+﻿using ExploringMars.Entities;
+
+namespace Application.Services;
+
+public static class ExplorationService
+{
+    public static void ExecuteRoversCommands(Plateau plateau)
+    {
+
+    }
+}
